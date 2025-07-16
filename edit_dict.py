b37a1ff4ec9c58115b7cce94ddd312d02dcbc24a
Kelvin Dela Cruz <kelvin.dcruz@gmail.com>
2025-07-16 13:45:17 -0500

    Completed [Chapter: Functions] in [Course: Learn JavaScript]

--- edit_dict.py
+++ edit_dict.py
@@ -1,10 +1,15 @@
 def update_data(data):
+    # Edit Below Here:
+    course_title = "Learn JavaScript"
+    chapter_title = "Functions"
+
+    # Don't touch below this
     for course_dict in data:
-        if course_dict["course_title"] == "Learn JavaScript":
+        if course_dict["course_title"] == course_title:
             for chapter_dict in course_dict["chapters"]:
-                if chapter_dict["chapter_title"] == "Comparisons":
+                if chapter_dict["chapter_title"] == chapter_title:
                     for lesson_dict in chapter_dict["lessons"]:
                         lesson_dict["completed"] = True
 
-    commit_message = "Completed [Comparisons] in [Learn JavaScript]" # Edit This 
+    commit_message = f"Completed [Chapter: {chapter_title}] in [Course: {course_title}]"
     return data, commit_message
