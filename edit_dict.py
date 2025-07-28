e0bdec5c50a01fdce333a465ad65f9e5d9b7d8ac
Kelvin Dela Cruz <kelvin.dcruz@gmail.com>
2025-07-28 18:09:35 -0500

    completed Arrays, Errors, Sets in Learn JS

--- edit_dict.py
+++ edit_dict.py
@@ -1,13 +1,13 @@
 def update_data(data):
     # Edit Below Here:
     course_title = "Learn JavaScript"
-    chapter_title = "Classes"
+    chapter_title = ["Arrays", "Errors", "Sets"]
 
     # Don't touch below this
     for course_dict in data:
         if course_dict["course_title"] == course_title:
             for chapter_dict in course_dict["chapters"]:
-                if chapter_dict["chapter_title"] == chapter_title:
+                if chapter_dict["chapter_title"] in chapter_title:
                     for lesson_dict in chapter_dict["lessons"]:
                         lesson_dict["completed"] = True
 
