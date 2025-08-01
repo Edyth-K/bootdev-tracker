147f4cefc067c94dfc03340b7b208ef05eed76f1
Edyth-K <kelvin.dcruz@gmail.com>
2025-08-01 18:21:42 -0500

    Completed [Chapter: The Event Loop] in [Course: Learn JavaScript]

--- edit_dict.py
+++ edit_dict.py
@@ -1,7 +1,7 @@
 def update_data(data):
     # Edit Below Here:
     course_title = "Learn JavaScript"
-    chapter_title = "Promises"
+    chapter_title = "The Event Loop"
 
     # Don't touch below this
     for course_dict in data:
