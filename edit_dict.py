a0ac1719d5975779b941d29bb79bfd9cb37f3636
Edyth-K <kelvin.dcruz@gmail.com>
2025-08-01 18:21:08 -0500

    Completed [Chapter: Promises] in [Course: Learn JavaScript]

--- edit_dict.py
+++ edit_dict.py
@@ -1,7 +1,7 @@
 def update_data(data):
     # Edit Below Here:
     course_title = "Learn JavaScript"
-    chapter_title = "Maps"
+    chapter_title = "Promises"
 
     # Don't touch below this
     for course_dict in data:
